b861aad22728a3161761ef05891b62eea7fab84b
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-29 18:04:17 +0200

    [FIX] corretto eager loader

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,14 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Project;
-use App\Models\Technology;
-use App\Models\Type;
+
 
 class ProjectController extends Controller
 {
     public function index (){
         // $projects = Project::all();
-        $projects= Project::with('technology','type')->get();
+        $projects = Project::with( 'type', 'technologies')->get();
         return response()->json([
             'success'=> true,
             'results'=> $projects
