914a261876a5f547801a60d0df36e8682e14f8fb
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-29 16:10:12 +0200

    [FEAT] milestone 2 - chiamata api funzionante

--- routes/api.php
+++ routes/api.php
@@ -20,20 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// TESTATA E FUNZIONANTE
 Route::get('/projects', [ProjectController::class, 'index']);
 
 
 
-// FUNZIONANTE
-// Route::get('/test', function(){
-   
-
-//         $projects = Project::all();
-
-//         return response()->json([
-//             'success'=> true,
-//             'results'=> $projects,
-//         ]);
-    
-// });
