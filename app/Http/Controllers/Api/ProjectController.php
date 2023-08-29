f9062aa760c31aaf2d313523451383482a86e9e0
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-29 17:35:09 +0200

    [FEAT] settaggio eager loading

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 
 class ProjectController extends Controller
 {
     public function index (){
-        $projects = Project::all();
+        // $projects = Project::all();
+        $projects= Project::with('technology','type')->get();
         return response()->json([
             'success'=> true,
             'results'=> $projects
