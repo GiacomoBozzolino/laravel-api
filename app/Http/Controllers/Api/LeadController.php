4291f7b38ca6a982d42bfba66506b414e43402df
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-09-01 14:35:08 +0200

    [FEAT] creazione migration Lead

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+
+
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\Validator;
+use Illuminate\support\Facedes\Mail;
+use App\Models\Lead;
+use App\Mail\NOME_CLASSE;
+
+class LeadController extends Controller
+{
+    //
+}
